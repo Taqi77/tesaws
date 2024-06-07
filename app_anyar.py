1c59e0615ad51c6615d1afb946a5c94d00c9c8d4
Taqi77 <83162053+Taqi77@users.noreply.github.com>
2024-06-08 00:20:00 +0700

    Add files via upload

---
+++ app_anyar.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Welcome to my simple web service!"
+
+@app.route('/hello')
+def hello():
+    return "Hello, World!"
+
+@app.route('/data')
+def data():
+    sample_data = {"name": "John", "age": 30, "city": "New York"}
+    return jsonify(sample_data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
+
